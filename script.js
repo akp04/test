6d802539f57efd5ac152ca3afaf0824c4b5bf876
AAKASH PANDITA <aakash2711522@gmail.com>
2020-09-12 18:06:05 +0530

    testing 1
    testing 1

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function loadDoc() {     
             //alert("fetching get request");
             var xhttp = new XMLHttpRequest();
-            var url = "http://localhost:3000/products";
+            var url = "server/players";
+
             xhttp.onreadystatechange = function () {
                 if (xhttp.readyState == 4 /*&& xhttp.status == 200 */ ) {
                     var jsonArr = JSON.parse(xhttp.responseText);
                     //showArr(jsonArr);
-                    console.log(jsonArr);
+                    //console.log(jsonArr);
+                    document.getElementById("showData").innerHtml= "Done!";
                 }
             };
             xhttp.open('GET',url,true);
